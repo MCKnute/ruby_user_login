17e02aa6549ce9e467713cd992d2ac7095e4a6c7
Mollie Knute <mcknute@gmail.com>
2016-03-22 14:15:30 -0700

    Completed login assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:errors] = ["Invalid combination with wrong username and/or wrong password"]
+      redirect_to new_session_path
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  get 'sessions/new' => 'sessions#new', as: :new_session
+  post 'sessions' => 'sessions#create'
+  get 'users/:id' => 'users#show', as: :user
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
